783d2e740f720d09754d3ed53f17ea0f9f482b41
mpietraszuk <GitHub@troz.me>
2024-01-03 07:57:34 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Svelte Feedback App
+
+```bash
+
+# Install dependencies
+npm install
+
+# Dev server
+npm run dev (http://localhost:5000)
+
+# Prod server
+npm start
+
+# Build for production
+npm run build
+```
---
+++ src/stores.js
@@ -0,0 +1,19 @@
+import { writable } from 'svelte/store'
+
+export const FeedbackStore = writable([
+		{
+			id: 1,
+			rating: 10,
+			text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+		},
+		{
+			id: 2,
+			rating: 9,
+			text: 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+		},
+		{
+			id: 3,
+			rating: 8,
+			text: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.',
+		},
+])
